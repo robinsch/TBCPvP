b32a773ed207c0a305a155e2cf465128b077c159
robinsch <robin.schriever.hude@web.de>
2016-05-04 13:42:22 +0200

    DB/AI: Added AI Shattered Hand Neophyte (Entry: 19410)

--- sql/updates/world/2016_xx_xx_dev_creature_ai_scripts.sql
+++ sql/updates/world/2016_xx_xx_dev_creature_ai_scripts.sql
@@ -22,3 +22,16 @@ UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=@ENTRY;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
 (@ENTRY*100+1, @ENTRY, 4, 0, 100, 1, 0, 0, 0, 0, 11, 32723, 0, 1, 43, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Raider - Cast Summon Bonechewer Riding Wolf and Dismount on Aggro');
+
+-- Shattered Hand Neophyte
+SET @ENTRY := 19410;
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=@ENTRY;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
+INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
+(@ENTRY*100+1, @ENTRY, 0, 0, 100, 0, 4000, 4500, 16000, 17000, 11, 11639, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Shadow Word: Pain'),
+(@ENTRY*100+2, @ENTRY, 2, 0, 100, 1, 30, 0, 0, 0, 11, 6742, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Bloodlust at 30% HP'),
+(@ENTRY*100+3, @ENTRY, 16, 0, 100, 1, 6742, 30, 22000, 25000, 11, 6742, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Bloodlust on Friendlies Missing Buff'),
+(@ENTRY*100+4, @ENTRY, 2, 0, 100, 1, 50, 0, 0, 0, 11, 11640, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Renew at 50% HP'),
+(@ENTRY*100+5, @ENTRY, 2, 0, 100, 0, 15, 0, 0, 0, 25, 0, 0, 0, 1, -47, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Flee at 15% HP'),
+(@ENTRY*100+6, @ENTRY, 6, 0, 10, 0, 0, 0, 0, 0, 11, 38879, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Soul Burden on Death'); -- Needs core support
+
