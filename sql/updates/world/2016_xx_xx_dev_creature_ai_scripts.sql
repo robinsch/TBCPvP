619ac7293454c8c1a598cdb38cb9c1721ffa8a31
robinsch <robin.schriever.hude@web.de>
2016-05-04 16:41:26 +0200

    DB/AI: Added AI Shattered Hand Grunt (Entry: 16867)
    Changed event_type repeat

--- sql/updates/world/2016_xx_xx_dev_creature_ai_scripts.sql
+++ sql/updates/world/2016_xx_xx_dev_creature_ai_scripts.sql
@@ -3,8 +3,8 @@ SET @ENTRY := 18952;
 UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=@ENTRY;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
-(@ENTRY*100+1, @ENTRY, 0, 0, 100, 0, 2000, 4000, 5000, 8000, 11, 34113, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Scavenger - Cast Bonechewer Bite'),
-(@ENTRY*100+2, @ENTRY, 4, 0, 100, 1, 0, 0, 0, 0, 11, 13398, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Scavenger - Cast Throw Wrench on Aggro'),
+(@ENTRY*100+1, @ENTRY, 0, 0, 100, 1, 2000, 4000, 5000, 8000, 11, 34113, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Scavenger - Cast Bonechewer Bite'),
+(@ENTRY*100+2, @ENTRY, 4, 0, 100, 0, 0, 0, 0, 0, 11, 13398, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Scavenger - Cast Throw Wrench on Aggro'),
 (@ENTRY*100+3, @ENTRY, 2, 0, 100, 0, 15, 0, 0, 0, 25, 0, 0, 0, 1, -47, 0, 0, 0, 0, 0, 0, 'Bonechewer Scavenger - Flee at 15% HP');
 
 -- Bonechewer Mutant
@@ -12,9 +12,9 @@ SET @ENTRY := 16876;
 UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=@ENTRY;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
-(@ENTRY*100+1, @ENTRY, 0, 0, 100, 0, 2000, 4000, 5000, 8000, 11, 34113, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Mutant - Cast Bonechewer Bite'),
-(@ENTRY*100+2, @ENTRY, 2, 0, 100, 1, 30, 0, 0, 0, 11, 8599, 0, 1, 1, -46, 0, 0, 0, 0, 0, 0, 'Bonechewer Mutant - Cast Enrage and Say Text at 30% HP'),
-(@ENTRY*100+3, @ENTRY, 2, 0, 100, 1, 20, 0, 0, 0, 11, 34114, 0, 1, 1, -149, 0, 0, 0, 0, 0, 0, 'Bonechewer Mutant - Cast Instability and Say Text at 20% HP');
+(@ENTRY*100+1, @ENTRY, 0, 0, 100, 1, 2000, 4000, 5000, 8000, 11, 34113, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bonechewer Mutant - Cast Bonechewer Bite'),
+(@ENTRY*100+2, @ENTRY, 2, 0, 100, 0, 30, 0, 0, 0, 11, 8599, 0, 1, 1, -46, 0, 0, 0, 0, 0, 0, 'Bonechewer Mutant - Cast Enrage and Say Text at 30% HP'),
+(@ENTRY*100+3, @ENTRY, 2, 0, 100, 0, 20, 0, 0, 0, 11, 34114, 0, 1, 1, -149, 0, 0, 0, 0, 0, 0, 'Bonechewer Mutant - Cast Instability and Say Text at 20% HP');
 
 -- Bonechewer Raider
 SET @ENTRY := 16925;
@@ -28,10 +28,19 @@ SET @ENTRY := 19410;
 UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=@ENTRY;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
-(@ENTRY*100+1, @ENTRY, 0, 0, 100, 0, 4000, 4500, 16000, 17000, 11, 11639, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Shadow Word: Pain'),
-(@ENTRY*100+2, @ENTRY, 2, 0, 100, 1, 30, 0, 0, 0, 11, 6742, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Bloodlust at 30% HP'),
+(@ENTRY*100+1, @ENTRY, 0, 0, 100, 1, 4000, 4500, 16000, 17000, 11, 11639, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Shadow Word: Pain'),
+(@ENTRY*100+2, @ENTRY, 2, 0, 100, 0, 30, 0, 0, 0, 11, 6742, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Bloodlust at 30% HP'),
 (@ENTRY*100+3, @ENTRY, 16, 0, 100, 1, 6742, 30, 22000, 25000, 11, 6742, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Bloodlust on Friendlies Missing Buff'),
 (@ENTRY*100+4, @ENTRY, 2, 0, 100, 1, 50, 0, 0, 0, 11, 11640, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Renew at 50% HP'),
 (@ENTRY*100+5, @ENTRY, 2, 0, 100, 0, 15, 0, 0, 0, 25, 0, 0, 0, 1, -47, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Flee at 15% HP'),
-(@ENTRY*100+6, @ENTRY, 6, 0, 10, 0, 0, 0, 0, 0, 11, 38879, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Soul Burden on Death'); -- Needs core support
+(@ENTRY*100+6, @ENTRY, 6, 0, 100, 0, 0, 0, 0, 0, 11, 38879, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Neophyte - Cast Soul Burden on Death'); -- Needs core support
 
+-- Shattered Hand Grunt
+SET @ENTRY := 16867;
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=@ENTRY;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
+INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES 
+(@ENTRY*100+1, @ENTRY, 9, 0, 100, 1, 0, 5, 8000, 9000, 11, 11976, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Grunt - Cast Strike'),
+(@ENTRY*100+2, @ENTRY, 0, 0, 100, 1, 6000, 8000, 12000, 14000, 11, 11978, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Grunt - Cast Kick'),
+(@ENTRY*100+3, @ENTRY, 2, 0, 100, 0, 15, 0, 0, 0, 25, 0, 0, 0, 1, -47, 0, 0, 0, 0, 0, 0, 'Shattered Hand Grunt - Flee at 15% HP'),
+(@ENTRY*100+4, @ENTRY, 6, 0, 100, 0, 0, 0, 0, 0, 11, 38879, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Shattered Hand Grunt - Cast Soul Burden on Death'); -- Needs core support
