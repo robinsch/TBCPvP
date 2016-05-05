cf34c24670910aba6e534d810ad31ec2526a0111
robinsch <robin.schriever.hude@web.de>
2016-05-05 20:08:02 +0200

    DB/Creature: Changed Thrallmar Grunt Faction https://github.com/TrinityCore/TrinityCore/commit/4c47ff3392ccb266f88db34835078abbb6d455ef

---
+++ sql/updates/world/2016_05_05_world_03.sql
@@ -0,0 +1,4 @@
+-- Thrallmar Grunt Faction
+UPDATE `creature_template` SET `faction_A`=1668, `faction_H`='1668' WHERE `entry` IN (16580, 16582);
+UPDATE `creature_template` SET `faction_A`=1671,  `faction_H`='1668' WHERE `entry` IN (16831);
+UPDATE `creature_template` SET `faction_A`=1666,  `faction_H`='1668' WHERE `entry` IN (16842);
