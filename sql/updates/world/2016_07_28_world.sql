f4a2f5eb16fbea27518556061e4705ec579b7854
robinsch <robin.schriever.hude@web.de>
2016-07-29 21:31:41 +0200

    Core/Spell: Fixed a typo in d727ab90e242343bad08490eee5efb56026e38a1

--- sql/updates/world/2016_07_28_world.sql
+++ sql/updates/world/2016_07_28_world.sql
@@ -10,8 +10,8 @@ UPDATE `spell_affect` SET `SpellFamilyMask`='12965527033218834' WHERE (`entry`='
 UPDATE `spell_affect` SET `SpellFamilyMask`='12965527033218834' WHERE (`entry`='17122') AND effectId = 1;
 
 -- Priest [Silent Resolve]
-DELETE FROM `spell_affect` WHERE entry in (14532, 14784, 14785, 14786, 14787) AND effectId = 1;
-INSERT INTO `spell_affect` VALUES(14532, 1, 24368649060719);
+DELETE FROM `spell_affect` WHERE entry in (14523, 14784, 14785, 14786, 14787) AND effectId = 1;
+INSERT INTO `spell_affect` VALUES(14523, 1, 24368649060719);
 INSERT INTO `spell_affect` VALUES(14784, 1, 24368649060719);
 INSERT INTO `spell_affect` VALUES(14785, 1, 24368649060719);
 INSERT INTO `spell_affect` VALUES(14786, 1, 24368649060719);
